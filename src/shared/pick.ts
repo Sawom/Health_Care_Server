6664eb41d83680d2afc2b4dca204718a89be4109
Sawom <asawom250@gmail.com>
2025-06-28 22:30:13 +0600

    pick and prisma updated for filtering

---
+++ src/shared/pick.ts
@@ -0,0 +1,17 @@
+// with pick function we use for filtering and searching
+
+const pick = <T extends Record<string, unknown>, k extends keyof T>(
+  obj: T,
+  keys: k[]
+) => {
+  const finalObj: Partial<T> = {};
+
+  for (const key of keys) {
+    if (obj && Object.hasOwnProperty.call(obj, key)) {
+      finalObj[key] = obj[key];
+    }
+  }
+  console.log(finalObj);
+  return finalObj;
+};
+export default pick;
