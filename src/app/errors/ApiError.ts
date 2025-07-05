44ec4e901ba9c799db89c77325bb0089c00b8d87
Sawom <asawom250@gmail.com>
2025-07-05 17:49:22 +0600

    user based route controll

--- src/app/errors/ApiError.ts
+++ src/app/errors/ApiError.ts
@@ -1,10 +1,12 @@
+// send api error style code is based on class. so we use class here
+// Error super class, ApiError child class
 class ApiError extends Error {
   statusCode: number;
   constructor(statusCode: number, message: string | undefined, stack = "") {
     super(message);
     this.statusCode = statusCode;
-    if (stack) {
-      this.stack = stack;
+    if (stack) { // Error class er 1ta stack thake
+      this.stack = stack; 
     } else {
       Error.captureStackTrace(this, this.constructor);
     }
