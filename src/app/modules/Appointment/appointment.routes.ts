f7d0c1d4472bce7564e27098d414bc7b57c8df3f
Sawom <asawom250@gmail.com>
2025-07-20 00:23:49 +0600

    appointment route cobntroller service

---
+++ src/app/modules/Appointment/appointment.routes.ts
@@ -0,0 +1,29 @@
+import { UserRole } from "@prisma/client";
+import express from "express";
+import auth from "../../middlewares/auth";
+import validateRequest from "../../middlewares/validateRequest";
+import { AppointmentController } from "./appointment.controller";
+import { AppointmentValidation } from "./appointment.validation";
+
+const router = express.Router();
+
+router.get(
+  "/",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AppointmentController.getAllFromDB
+);
+
+router.get(
+  "/my-appointment",
+  auth(UserRole.PATIENT, UserRole.DOCTOR),
+  AppointmentController.getMyAppointment
+);
+
+router.post(
+  "/",
+  auth(UserRole.PATIENT),
+  validateRequest(AppointmentValidation.createAppointment),
+  AppointmentController.createAppointment
+);
+
+export const AppointmentRoutes = router;
