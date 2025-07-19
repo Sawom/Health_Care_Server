e9ec651dc9876a4403691dec4924aadc950790c1
Sawom <asawom250@gmail.com>
2025-07-20 01:48:25 +0600

    central route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import { AdminRoutes } from "../modules/Admin/admin.routes";
+import { AppointmentRoutes } from "../modules/Appointment/appointment.routes";
 import { AuthRoutes } from "../modules/Auth/auth.routes";
 import { DoctorRoutes } from "../modules/Doctor/doctor.routes";
+import { DoctorScheduleRoutes } from "../modules/DoctorSchedule/doctorSchedule.routes";
+import { PatientRoutes } from "../modules/Patient/patient.route";
+import { ScheduleRoutes } from "../modules/Schedule/schedule.routes";
 import { SpecialtiesRoutes } from "../modules/Specialties/specialties.routes";
 import { userRoutes } from "../modules/User/user.routes";
 
@@ -27,6 +31,22 @@ const moduleRoutes = [
   {
     path: "/doctor",
     route: DoctorRoutes,
+  },
+  {
+    path: "/patient",
+    route: PatientRoutes,
+  },
+  {
+    path: "/schedule",
+    route: ScheduleRoutes,
+  },
+  {
+    path: "/doctor-schedule",
+    route: DoctorScheduleRoutes,
+  },
+  {
+    path: "/appointment",
+    route: AppointmentRoutes,
   },
 ];
 
