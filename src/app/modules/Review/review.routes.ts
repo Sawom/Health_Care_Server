cf7e1dfd9e971e59096d742927bceb0859911cd2
Sawom <asawom250@gmail.com>
2025-07-26 17:40:11 +0600

    review module added

---
+++ src/app/modules/Review/review.routes.ts
@@ -0,0 +1,19 @@
+import { UserRole } from "@prisma/client";
+import express from "express";
+import auth from "../../middlewares/auth";
+import validateRequest from "../../middlewares/validateRequest";
+import { ReviewController } from "./review.controller";
+import { ReviewValidation } from "./review.validation";
+
+const router = express.Router();
+
+router.get("/", ReviewController.getAllFromDB);
+
+router.post(
+  "/",
+  auth(UserRole.PATIENT),
+  validateRequest(ReviewValidation.create),
+  ReviewController.insertIntoDB
+);
+
+export const ReviewRoutes = router;
